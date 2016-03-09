2cd0c873c2368368f6fbbafe054e838b3d78441e
jjkd <jjkd@jjkd.com>
2016-03-09 14:43:13 -0600

    Added graphics for buttons, orientation on info page

---
+++ app/src/main/java/com/example/jjkd/whyknots/Knot.java
@@ -0,0 +1,18 @@
+package com.example.jjkd.whyknots;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+/**
+ * Created by jjkd on 3/9/2016.
+ */
+public class Knot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.knot);
+    }
+}
