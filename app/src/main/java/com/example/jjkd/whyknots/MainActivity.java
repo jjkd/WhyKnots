e4b6012742a71d5e8b2dd661e5dc69bcbd74b9f8
jjkd <jjkd@jjkd.com>
2016-03-09 11:58:43 -0600

    Initial commit

---
+++ app/src/main/java/com/example/jjkd/whyknots/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.jjkd.whyknots;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void invokeButton(View view) {
+        Intent intent = new Intent(this, Info.class);
+        startActivity(intent);
+    }
+
+    public void invokeButton1(View view) {
+
+    }
+
+    public void invokeButton2(View view) {
+
+    }
+
+    public void invokeButton3(View view) {
+
+    }
+
+    public void invokeButton4(View view) {
+
+    }
+
+    public void invokeButton5(View view) {
+
+    }
+
+    public void invokeButton6(View view) {
+
+    }
+}
